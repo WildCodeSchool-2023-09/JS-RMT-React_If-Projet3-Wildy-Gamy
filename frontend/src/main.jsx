003a4e4e3af866b198609d7c205e8620980502de
jean519 <129189905+jean519@users.noreply.github.com>
2023-12-13 12:16:09 +0100

    initialisation du css,dependence et back

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./style/index.scss";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
