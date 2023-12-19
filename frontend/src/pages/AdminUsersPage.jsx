4d6fdf1cf781c558a4430bd28762f4b339082ced
Defreitasnicolas <defreitasnicolas@gmail.com>
2023-12-19 10:05:04 +0100

    creation de la page usersAdmin et du path

---
+++ frontend/src/pages/AdminUsersPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DeleteButton from "../components/DeleteButton";
+
+function AdminUsersPage() {
+  return (
+    <>
+      <div>AdminUsersPage</div>
+      <DeleteButton />
+    </>
+  );
+}
+
+export default AdminUsersPage;
