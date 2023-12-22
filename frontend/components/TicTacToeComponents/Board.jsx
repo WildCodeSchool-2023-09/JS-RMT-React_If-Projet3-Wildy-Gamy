0905e0a51ae906a4773c41e097cc8731d16067b8
sebastien <romeo.sebastien.dev@gmail.com>
2023-12-22 09:18:28 +0100

    jeu tictactoe

---
+++ frontend/components/TicTacToeComponents/Board.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Square from "./Square";
+
+function Board({ squares, onClick }) {
+  return (
+    <div className="board">
+      {squares.map((square, id) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <Square key={id} value={square} onClick={() => onClick(id)} />
+      ))}
+    </div>
+  );
+}
+
+Board.propTypes = {
+  squares: PropTypes.arrayOf(PropTypes.string).isRequired,
+
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Board;
