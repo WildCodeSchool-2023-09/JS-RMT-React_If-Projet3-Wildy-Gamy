35b3b668fd7cbf4a9b7e4a0bdeb5eb9d764609df
Defreitasnicolas <defreitasnicolas@gmail.com>
2024-01-03 16:33:20 +0100

    espacement sur adminUserTable et ajout de la fonction dans le jsx

--- frontend/src/components/AdminUsersTable.jsx
+++ frontend/src/components/AdminUsersTable.jsx
@@ -6,6 +6,7 @@ import connexion from "../../connexion";
 
 function AdminUsersTable({ search }) {
   const [userData, setUserData] = useState([]);
+
   const getUsers = async () => {
     try {
       const res = await connexion.get(`/players?search=${search}`);
@@ -14,6 +15,7 @@ function AdminUsersTable({ search }) {
       console.error(error);
     }
   };
+
   useEffect(() => {
     getUsers();
   }, [search]);
@@ -24,6 +26,7 @@ function AdminUsersTable({ search }) {
       console.error(error);
     }
   };
+
   return (
     <table className="table">
       <thead>
--- frontend/src/components/InputSearchBar.jsx
+++ frontend/src/components/InputSearchBar.jsx
@@ -2,18 +2,13 @@ import "../style/components/InputSearchBar.scss";
 import PropTypes from "prop-types";
 
 function InputSearchBar({ setSearch }) {
-  const handleSearch = (e) => {
-    const result = e.target.value;
-    setSearch(result);
-  };
-
   return (
     <div className="search-container">
       <input
         className="search-bar"
         type="text"
         placeholder="Search..."
-        onChange={handleSearch}
+        onChange={(e) => setSearch(e.target.value)}
       />
     </div>
   );
