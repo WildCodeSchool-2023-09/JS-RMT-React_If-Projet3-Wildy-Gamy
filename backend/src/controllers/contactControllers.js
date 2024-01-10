74e5367bfa53303cc594c740bb2573ead744f1b1
sebastien <romeo.sebastien.dev@gmail.com>
2024-01-10 11:09:49 +0100

    envoi backend ok

--- backend/src/controllers/contactControllers.js
+++ backend/src/controllers/contactControllers.js
@@ -7,10 +7,18 @@ const send = async (req, res, next) => {
       to: "romeo.sebastien.dev@gmail.com",
       subject: req.body.subject,
       text: req.body.message,
-      html: "<b>Hello world?</b>",
+      html: `<main>
+        <h1>Bonjour Administrateur</h1>
+        <h3>Vous avez un nouveau message de ${req.body.name} ${req.body.surname}</h3>
+        <h4>Téléphone facultatif : $${req.body.number}</h4>
+        <br /><span>____________________________________________________________</span>
+        <p>${req.body.message}</p>
+        <br /><span>____________________________________________________________</span>
+        <h5>Bonne journée</ h5>
+      </main >`,
     });
 
-    res.json({ msg: "votre email a bien été envoyé" });
+    res.status(200).json({ msg: "votre email a bien été envoyé" });
   } catch (err) {
     next(err);
   }
