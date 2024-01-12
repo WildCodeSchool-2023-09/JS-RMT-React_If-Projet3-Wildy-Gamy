6abbf6c0c9e95bfaf9c501575b9fbd8c429778b1
Defreitasnicolas <defreitasnicolas@gmail.com>
2024-01-12 12:26:11 +0100

    mise en place du hashPassword

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,11 +7,13 @@ const PartyControllers = require("./controllers/partyControllers");
 const GameControllers = require("./controllers/gameControllers");
 const ContactControllers = require("./controllers/contactControllers");
 
+const hashPassword = require("./midleware/hash/hashPassword");
+
 router.get("/players", PlayerControllers.browse);
 router.delete("/players/:id", PlayerControllers.destroy);
 router.get("/party", PartyControllers.browse);
 router.get("/games", GameControllers.browse);
 router.post("/contact", ContactControllers.send);
-router.post("/players", PlayerControllers.add);
+router.post("/players", hashPassword, PlayerControllers.add);
 
 module.exports = router;
