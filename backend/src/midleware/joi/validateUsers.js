516e0d7c55c4196997c89e5f9b168fb296741128
Defreitasnicolas <defreitasnicolas@gmail.com>
2024-01-12 12:38:41 +0100

    mise en place de midleware

---
+++ backend/src/midleware/joi/validateUsers.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+const schema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const validateUsers = (req, res, next) => {
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    res.status(422).json(error);
+  } else {
+    next();
+  }
+};
+
+module.exports = validateUsers;
--- backend/src/router.js
+++ backend/src/router.js
@@ -8,12 +8,13 @@ const GameControllers = require("./controllers/gameControllers");
 const ContactControllers = require("./controllers/contactControllers");
 
 const hashPassword = require("./midleware/hash/hashPassword");
+const validateUsers = require("./midleware/joi/validateUsers");
 
 router.get("/players", PlayerControllers.browse);
 router.delete("/players/:id", PlayerControllers.destroy);
 router.get("/party", PartyControllers.browse);
 router.get("/games", GameControllers.browse);
 router.post("/contact", ContactControllers.send);
-router.post("/players", hashPassword, PlayerControllers.add);
+router.post("/players", validateUsers, hashPassword, PlayerControllers.add);
 
 module.exports = router;
