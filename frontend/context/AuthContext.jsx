01b6ca2eee4a21f2dffadeaf8cb7abc3b3bbeeaa
jean519 <129189905+jean519@users.noreply.github.com>
2024-01-29 09:53:42 +0100

    ajout du context

--- frontend/context/AuthContext.jsx
+++ frontend/context/AuthContext.jsx
@@ -5,12 +5,16 @@ export const AuthContext = createContext();
 
 function AuthProvider({ children }) {
   const [connected, setConnected] = useState({ role: null });
+  const [profil, setProfil] = useState({ profiles: null });
+
   const context = useMemo(
     () => ({
       connected,
       setConnected,
+      profil,
+      setProfil,
     }),
-    [connected, setConnected]
+    [connected, setConnected, profil, setProfil]
   );
   return (
     <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
