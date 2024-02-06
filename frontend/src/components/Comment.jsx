99c1a8044baec187ece6ca81ed3af0cf2c1fca17
sebastien <romeo.sebastien.dev@gmail.com>
2024-02-06 17:08:35 +0100

    modif css comment

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -6,10 +6,12 @@ function Comment() {
   return (
     <div className="all-comment">
       <img className="icon-comment" src={icon} alt="icon" />
-      <Link to="/commentaires" className="comment-link">
-        Accéder au salon Tic-Tac-Toe
-      </Link>
-      <p className="comment-number">Nombre commentaires</p>
+      <div className="link-number">
+        <Link to="/commentaires" className="comment-link">
+          Accéder au salon Tic-Tac-Toe
+        </Link>
+        <p className="comment-number">Nombre commentaires</p>
+      </div>
     </div>
   );
 }
