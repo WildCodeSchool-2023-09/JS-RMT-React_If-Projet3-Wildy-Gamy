3ef9a91aebcaacfb2c982c58e1bad9346099b2e8
sebastien <romeo.sebastien.dev@gmail.com>
2024-02-07 12:16:30 +0100

    modif css page info et home

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo2.png";
 import ButtonConected from "../input/ButtonConected";
 import InputProfil from "../input/InputProfil";
 
 function Header() {
   return (
     <div className="headerContainer">
-      <img src={logo} alt="logo" width={100} />
+      <img src={logo} alt="logo" width={50} />
       <ButtonConected />
       <InputProfil />
     </div>
